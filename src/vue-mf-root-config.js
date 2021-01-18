9fe88b5af81a7db35bf9d87c7cb644ff705146a2
glennpacker <glenn.packer@glennpacker.net>
2021-01-18 16:19:58 +0000

    nav application added

--- src/vue-mf-root-config.js
+++ src/vue-mf-root-config.js
@@ -1,24 +1,15 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@vue-mf/navbar",
-  app: () => System.import("@vue-mf/navbar"),
+  name: "nav",
+  app: () => System.import("nav"),
   activeWhen: "/",
-  customProps: {
-    githubLink: "https://github.com/vue-microfrontends/root-config",
-  },
 });
 
 registerApplication({
   name: "@vue-mf/dogs-dashboard",
   app: () => System.import("@vue-mf/dogs-dashboard"),
   activeWhen: "/view-doggos",
-});
-
-registerApplication({
-  name: "@vue-mf/rate-dogs",
-  app: () => System.import("@vue-mf/rate-dogs"),
-  activeWhen: "/rate-doggos",
 });
 
 registerApplication({
